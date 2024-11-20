9842b8db331fb0fb7abef1a309db09dec991ea92
Kateryna Nuzhna <katranchik21@gmail.com>
2024-11-20 11:18:43 +0100

    init commit f

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ВВедіть назву файлу або папки: ");
+        String inputName = scanner.nextLine();
+
+        File file = new File(inputName);
+
+        if (file.exists()){
+            if (file.isFile()){
+                System.out.println(inputName + " - це файл.");
+                System.out.println("Розмір файлу: "+ file.length()+" байтів");
+            }else if (file.isDirectory()){
+                System.out.println(inputName + " -це папка" );
+                System.out.println("Вміст папки: ");
+                File [] files = file.listFiles();
+                if (files != null){
+                    for (File f:files){
+                        System.out.println(f.getName());
+                    }
+                }else {
+                    System.out.println("Папка пуста");
+                }
+            }
+        }else {
+            System.out.println(inputName + " в цій директорії проєкту не існує...");
+        }
     }
 }
